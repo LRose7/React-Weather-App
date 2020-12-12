2daf4ca240f6c5f029f918f4f50c09eeb96817b4
Lindsey <lindseyurquhart@tutanota.com>
2020-12-12 12:52:23 -0500

    dotenv

--- models/weather.js
+++ models/weather.js
@@ -1,11 +1,10 @@
 const request = require('request-promise');
-
-const API_KEY = 'b0f3e3cdb5e3577fbb18e1a813cc9606';
+require('dotenv').config();
 
 class Weather {
     static retrieveByCity (city, callback) {
         request({
-            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+            uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.API_KEY}&units=imperial`,
             json: true
         }).then((res) => {
             callback(res);
